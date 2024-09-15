d38f70a0748bdf2a39da6d2c9f1586e045516c4c
jao-o <nisperosjohn62@gmail.com>
2024-09-15 11:49:29 +0800

    finished add workouts

--- index.php
+++ index.php
@@ -2,19 +2,29 @@
 $servername = "localhost";
 $username = "root";
 $password = ""; 
-$dbname = "workout_tracker";  
-
+$dbname = "workout_tracker";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-echo "Connection to the database '$dbname' was successful!";
-
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $task_name = $_POST['task_name'];
+    $task_description = $_POST['task_description'];
+    
+    $stmt = $conn->prepare("INSERT INTO workout_tasks (task_name, task_description) VALUES (?, ?)");
+    $stmt->bind_param("ss", $task_name, $task_description);
+    
+    if ($stmt->execute()) {
+        echo "New workout task added successfully!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+    
+    $stmt->close();
+}
 
 $conn->close();
 ?>
